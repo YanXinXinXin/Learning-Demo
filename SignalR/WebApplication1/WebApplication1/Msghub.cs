ad199750c013b53601d0b1691e255050068d4a0f
username <320437031@qq.com>
2021-01-27 13:30:40 +0800

    SignalRDemo

---
+++ SignalR/WebApplication1/WebApplication1/Msghub.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class Msghub : Hub
+    {
+        //在hub方法中编写的方法，都是要被客户端调用的方法
+        public void SenMsg(string name,string txt)
+        {
+        
+
+            //服务器主动调用客户端的方法
+            Clients.All.getMsg(name,txt);
+
+            //除了自己 其他的人接收
+           // Clients.Others.getMsg(txt);
+
+        }
+    }
+}
